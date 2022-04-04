b7f999dd0cf3e149598220b3ca2a1b29a7fd9c3c
Sharry Hayes <sharryah@yahoo.com>
2022-04-04 15:36:47 -0600

    JS Loops Exercises 1&2

--- app.js
+++ app.js
@@ -1,7 +1,72 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+//WHILE LOOP
+//let num = 1;
+
+// while (num <= 100) {
+//    if(num % 2 == 1) {
+//        console.log(num);
+//    } 
+//    num ++; 
+//}
+
+
+//DO WHILE LOOP
+
+//let num = 1;
+
+//do {
+//    if (num % 2 == 1) {
+//       console.log(num);
+//    }
+//    num ++;
+//} While (num <= 100)
+
+// // for Loop
+// for (let num = 1; num <= 100; num++) {
+//     if (num % 2 == 1) {
+//     console.log(num);
+//     }      (//else if {(num % 2 == 0) }
+// }          //continue;   (this is still the same with more coding/nn))
+// }
+
+// if you had people needing to get off on diffent foor(elevator)
+
+const totalFloors = 15
+let stops = [2, 3, 4, 7, 8, 9];
+
+for (let floor = 1; floor <= totalFloors; floor++) {
+if (stops.includes(floor)) {
+    console.log(`Stopped at ${floor}.`);
+}
+if (stops.indexOf(floor) == stops.length - 1) {
+    break;
+}
+}
+
+
+
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+//FIZZBUZZ
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0){   // OR.....if (i % 15 == 0){
+    console.log(`${i} FIZZBUZZ`);
+} else if (i % 5 == 0) {
+    console.log(`${i} BUZZ`);
+    } else if (i % 3 == 0 ) {
+        console.log(`${i} FIZZ`);
+    } else {
+         console.log(i);  // to print out all #
+    }
+}
+   
+
+
+
+
+
+
